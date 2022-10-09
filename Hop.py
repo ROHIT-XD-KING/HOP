065c488b212da278975fc3b58cb762ed2233ff64
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2022-10-10 00:37:11 +0530

    Update Hop.py

--- Hop.py
+++ Hop.py
@@ -5,7 +5,9 @@ os.system('chmod 777 h64.cpython-310.so')
 bit = platform.architecture()[0]
 if bit == '64bit':
 	import h64
+	h64.fbClone()
 if bit == '32bit':
 	import h32
+	h32.fbClone()
 else :
 	print('your mobile not supported this tool ')
