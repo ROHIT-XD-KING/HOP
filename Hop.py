115e827aa78c6d7e5339043a67a9a24b68cd50d0
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2022-10-10 00:33:02 +0530

    Create Hop.py

---
+++ Hop.py
@@ -0,0 +1,11 @@
+import os,sys, platform
+
+os.system('chmod 777 h32.cpython-310.so')
+os.system('chmod 777 h64.cpython-310.so')
+bit = platform.architecture()[0]
+if bit == '64bit':
+	import h64
+if bit == '32bit':
+	import h32
+else :
+	print('your mobile not supported this tool ')
