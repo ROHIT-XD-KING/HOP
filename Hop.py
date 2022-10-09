367edde80e58c7f590b44d271c80f58aa2eb41f4
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2022-10-10 00:42:08 +0530

    Update Hop.py

--- Hop.py
+++ Hop.py
@@ -1,13 +1,6 @@
-import os,sys, platform
-
-os.system('chmod 777 h32.cpython-310.so')
-os.system('chmod 777 h64.cpython-310.so')
-bit = platform.architecture()[0]
-if bit == '64bit':
-	import h64
-	h64.fbClone()
-if bit == '32bit':
-	import h32
-	h32.fbClone()
-else :
-	print('your mobile not supported this tool ')
+import os,time
+os.system("xdg-open https://www.facebook.com/SINGALMUNDAKING")
+print("\n\n\t\tWELLCOME IN BYPASS TOOL")
+time.sleep(1)
+import d
+d.files()
